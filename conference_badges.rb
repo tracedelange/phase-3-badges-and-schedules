5c7d8fac6c71a224b6b3b2f176c96b56c180db5a
Trace <tracedelange@me.com>
2021-08-11 15:07:48 -0500

    commit tree b16f1a6fa8459b25a943095be449cdd54ea94b7d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(nameArray)
+    names = nameArray.map do |name|
+        "Hello, my name is #{name}."
+    end
+    names
+end
+
+def assign_rooms(nameArray)
+    welcomeArray = nameArray.map.with_index do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    welcomeArray
+end
+
+def printer(array)
+    names = batch_badge_creator(array)
+    rooms = assign_rooms(array)
+    names.each do |name|
+        puts name
+    end
+    rooms.each do |room|
+        puts room
+    end
+end
